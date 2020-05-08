41114ca0192428ca5e28e9aaa74dbf04eb55a63a
sid <siddbanerjee13@gmail.com>
2020-05-08 14:32:51 -0700

    empty plugin

---
+++ src/main/java/com/cyberdev/cyberplugin/CyberPlugin.java
@@ -0,0 +1,17 @@
+package com.cyberdev.cyberplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CyberPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
