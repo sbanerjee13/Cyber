f81391527c3600b252d9042189a741f6f83779b9
sid <siddbanerjee13@gmail.com>
2020-05-08 14:49:01 -0700

    Added basic credit command.

--- src/main/java/com/cyberdev/cyberplugin/CyberPlugin.java
+++ src/main/java/com/cyberdev/cyberplugin/CyberPlugin.java
@@ -1,5 +1,6 @@
 package com.cyberdev.cyberplugin;
 
+import com.cyberdev.cyberplugin.commands.*; // import all commands
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CyberPlugin extends JavaPlugin {
@@ -8,6 +9,8 @@ public final class CyberPlugin extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+        // handle commands
+        getCommand("credits").setExecutor(new credits());
     }
 
     @Override
